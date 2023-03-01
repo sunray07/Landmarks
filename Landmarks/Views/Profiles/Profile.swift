8fd56d6fbb5955e0c1b002c7b3ff8963d151e05e
Ibragim <wrestler20@icloud.com>
2023-03-01 14:24:23 +0300

    project loading

---
+++ Landmarks/Views/Profiles/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Ибрагим Сахратулаев on 01.03.2023.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "g_kumar")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+}
